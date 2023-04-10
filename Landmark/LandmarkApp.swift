55204c2d45e52ff55e08fa2a7ea50f31850d1fee
Steven <webdeploymentnf@gmail.com>
2023-04-10 20:59:06 +0200

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by steven rezac on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
