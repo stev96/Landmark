e85069712f251aec6ac9f57130fc70bfdccc920f
Steven <rezac.s96@gmail.com>
2023-04-14 14:13:40 +0200

    Detail Ansicht hinzugefügt

--- Landmark/View/ContentView.swift
+++ Landmark/View/ContentView.swift
@@ -9,36 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircelImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack (alignment: .leading) {
-                Text("Turtel Rock")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                
-                Text("About Turtel Rock")
-                    .font(.title2)
-                Text("Descriptive text goes brrrrrr.....")
-            }
-            .padding()
-            
-            Spacer()
-        }
+       LandmarkList()
     }
 }
 
